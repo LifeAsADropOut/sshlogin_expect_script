835c03ac3847f8f8519d7cb3542c9f7cd17e79a1
LifeAsADropOut <vksharma.2559@gmail.com>
2021-07-19 07:09:02 +0530

    Update sshl.sh

--- sshl.sh
+++ sshl.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/expect -f
 
-# # trunk(){
-
-# # 	read _arg
-
-# # 	local _re="$( echo $_arg | tr -d ":" )" 
-# # 	echo $_re
-# # }
-
-
-# declare -r SERV="bandit.labs.overthewire.org"
-# declare -r PORT=2220
-
-# read -p "ENTER NAME:PAS -> " arg
-
-# level="$( echo $arg | grep "[a-z0-9]*:" | sed 's/:[a-z0-9A-Z]*//' )"
-# pas="$( echo $arg |  grep ":[a-z0-9A-Z]*" | sed 's/[a-z0-9A-Z]*://' )"
-
-# echo $pas $level
-# ssh -tt "$level@$SERV" -p $PORT<<EOT 
-# "$pas"
-# EOT
-
 set timeout 60
 
 set PORT 2220
